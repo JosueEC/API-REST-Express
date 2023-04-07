39930e16c32ffd78e3e152de49b0337cee2778f7
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-06 21:10:33 -0600

    Se agregaron comentarios descriptivos al modulo find-all-chracters

--- src/controllers/character-controllers/find-all-characters.js
+++ src/controllers/character-controllers/find-all-characters.js
@@ -19,3 +19,13 @@ const findAllCharacters = async (query) => {
 }
 
 module.export = findAllCharacters
+
+// La propiedad where: query trae datos que vienen en la peticion
+// con estos datos se realizan filtros en nuestra busqueda
+// include: Es el equivalente al join de SQl en sequelize, con esta
+// propiedad estamos indicando que en el resultado que nos devuelve
+// nuestra busqueda, queremos que se incluyan los datos del modelo
+// Episode que tenga asociado
+// attributes: Con esta propiedad establecemos que columnas queremos
+// que se retornen en el resultado, esto es en el caso de que solo
+// queremos ciertos datos de el o los registros que estamos pidiendo
